9adc7b88bdd3f0c7f35d8a28dc2148f366b3d195
dolores91 <doloresalemang@gmail.com>
2023-04-04 13:52:13 -0300

    favorites working

--- src/paginas/Favoritos.pagina.jsx
+++ src/paginas/Favoritos.pagina.jsx
@@ -1,4 +1,7 @@
 import GrillaPersonajes from "../componentes/personajes/grilla-personajes.componente";
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import {  guardarFavoritos } from '../redux/gallerySlice.ts';
+
 
 /**
  * Esta es la pagina de favoritos. Aquí se deberan ver todos los personajes marcados como favoritos
@@ -9,12 +12,33 @@ import GrillaPersonajes from "../componentes/personajes/grilla-personajes.compon
  * @returns la pagina de favoritos
  */
 const PaginaFavoritos = () => {
+    const dispatch = useAppDispatch();
+    const personajes = useAppSelector((state) => state.gallery.tarjetas);
+    const favoritos = useAppSelector((state) => state.gallery.favoritos);
+
+ //favoritos
+ const onFavorite= (id) => {
+    const isId = favoritos.some((favorito) => favorito === id);
+    if (isId) {
+        const updateId = favoritos.filter((favorito) => favorito !== id);
+        dispatch(guardarFavoritos(updateId));
+    } else {
+        dispatch(guardarFavoritos([...favoritos, id]));
+    }
+};
+//console.log(personajes);
+const personajesFiltrados = personajes.filter(personajes => favoritos.includes(personajes.id));
+console.log(personajesFiltrados);
+
     return <div className="container">
         <div className="actions">
             <h3>Personajes Favoritos</h3>
             <button className="danger">Test Button</button>
         </div>
-        <GrillaPersonajes />
+        <GrillaPersonajes
+         personajes={personajesFiltrados}
+            onclick={onFavorite}
+            favoritos={favoritos} />
     </div>
 }
 
